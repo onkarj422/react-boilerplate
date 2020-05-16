40e782da55213f81ea98505a3e01475c8e154963
Onkar Joshi <onkar.joshi@energyexemplar.com>
2020-05-16 20:03:29 +0530

    remove unwanted

--- .eslintrc.js
+++ .eslintrc.js
@@ -75,8 +75,6 @@ module.exports = {
     'react/require-extension': 0,
     'react/self-closing-comp': 0,
     'react/sort-comp': 0,
-    'redux-saga/no-yield-in-race': 2,
-    'redux-saga/yield-effects': 2,
     'require-yield': 0,
   },
   settings: {
